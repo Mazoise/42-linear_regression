b9981635bfb09d57b6ee788b76b26755a4d5a612
mchardin <mchardin@student.42.fr>
2022-07-06 18:02:15 +0200

    oups

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ try:
     data = pd.read_csv("data.csv")
     Mileage = np.array(data["km"]).reshape(-1, 1)
     Price = np.array(data["price"]).reshape(-1, 1)
-    open_file = open("thetas.txt", "w")
     myLR = MyLr(np.array([[0], [0]]), 1e-1, 10000)
     print(myLR.fit_(Mileage, Price))
     model = { "thetas": myLR.thetas.squeeze(), "bounds": myLR.bounds }
